051e5cb989423c112dc0a6df7ad32a8d9e4a0ecd
Christopher Jenkins <chris.jenkins@lvt.com>
2023-08-24 13:50:33 -0600

    remove unnecessary mock

--- components/ContinentScreen/__test__/ContinentScreen.test.jsx
+++ components/ContinentScreen/__test__/ContinentScreen.test.jsx
@@ -1,17 +1,6 @@
 import ContinentScreen from "../ContinentScreen";
 import { shallow } from "../../../utils/test/shallow";
 
-
-jest.mock("react-native-safe-area-context", () => ({
-  useSafeAreaInsets: () => ({
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-  }),
-}));
-
-
 it("should render", () => {
   const content = shallow(<ContinentScreen/>);
 
